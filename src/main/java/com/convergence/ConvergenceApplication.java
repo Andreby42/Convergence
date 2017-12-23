38b94f6c1f6b875fd340c20782a00a9b9ff18284
yberdna <38912428@qq.com>
2017-12-23 23:50:55 +0800

    TODO:多数据源实现

--- src/main/java/com/convergence/ConvergenceApplication.java
+++ src/main/java/com/convergence/ConvergenceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Import;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
