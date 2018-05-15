bf179ffdb9c85984997fb109674a0bd3ee2de493
andreby42 <38912428@qq.com>
2018-05-15 16:48:29 +0800

    bloomFilter guava

--- src/main/java/com/convergence/support/structrue/BloomFilterTest.java
+++ src/main/java/com/convergence/support/structrue/BloomFilterTest.java
@@ -17,7 +17,12 @@ public class BloomFilterTest {
             BloomFilter.create(Funnels.stringFunnel(Charset.defaultCharset()), sizeOfNumberSet);
     static BloomFilter<Integer> intFilter =
             BloomFilter.create(Funnels.integerFunnel(), sizeOfNumberSet);
-
+    //三参数 第三个参数为允许的错误率
+    static BloomFilter<Integer> intFilters =
+            BloomFilter.create(Funnels.integerFunnel(), sizeOfNumberSet,0.00001);
+    //四参数 第四个参数为哈希算法 默认算法为BloomFilterStrategies.MURMUR128_MITZ_64
+    static BloomFilter<Integer> intFilterss =
+            BloomFilter.create(Funnels.integerFunnel(), sizeOfNumberSet,0.00001);
     public static void testBloomFilter() {
         boolean flag1 = filter.put("123");
         boolean flag2 = filter.put("123");
@@ -26,6 +31,7 @@ public class BloomFilterTest {
 
         boolean flag3 = intFilter.put(123);
         boolean flag4 = intFilter.put(123);
+        //使用mightContain 来判断是否存在
         boolean flag5 = intFilter.mightContain(123);
         // intFilter.readFrom(in, funnel)
         System.out.println(flag3);
