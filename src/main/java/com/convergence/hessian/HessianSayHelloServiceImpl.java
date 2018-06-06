b9fc42b79b31b880c518eea5c330abc118c5eadd
andreby <38912428@qq.com>
2018-06-06 23:36:45 +0800

    no message

--- src/main/java/com/convergence/hessian/HessianSayHelloServiceImpl.java
+++ src/main/java/com/convergence/hessian/HessianSayHelloServiceImpl.java
@@ -2,7 +2,10 @@ package com.convergence.hessian;
 
 import org.springframework.stereotype.Service;
 
+import com.caucho.hessian.HessianUnshared;
+
 @Service("hessianSayHelloService")
+@HessianUnshared
 public class HessianSayHelloServiceImpl implements HessianSayHelloService {
 
 	@Override
